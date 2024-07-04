0044df24a83069853d7f1a09b4ee8906c25c258d
MohammadJavad <ghorbanimohammadjavad969@gmail.com>
2024-07-04 15:46:35 +0430

    home page created

--- Vira_Project/settings.py
+++ Vira_Project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_render_partial',
+    'home_module.apps.HomeModuleConfig',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
 TIME_ZONE = 'UTC'
 
@@ -116,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- home_module/views.py
+++ home_module/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_page(request):
+    return render(request , 'home.html' , {
+
+    })
+
+def header_render_partial(request):
+    return render(request , 'header.html' , {
+
+    })
+
+def footer_render_partial(request):
+    return render(request , 'footer.html' , {
+
+    })
